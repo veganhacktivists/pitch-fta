40d60bb3edd3521c78e6d6ced2e4a76b57248cd5
Gerard O'Neill <grardb@gmail.com>
2022-10-15 20:49:28 +0200

    Prevent zooming in on mobile

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
         <link rel="manifest" href="/manifest.json">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
