387241f32c88c2f0ea9529b5ca20b3b1675a65aa
Gerard O'Neill <grardb@gmail.com>
2022-10-15 11:59:49 +0200

    Fix modal width on desktop devices

--- resources/js/Components/Modal.tsx
+++ resources/js/Components/Modal.tsx
@@ -1,6 +1,7 @@
 import { Dialog, Transition, TransitionEvents } from '@headlessui/react'
 import React, { Fragment } from 'react'
 import { ParentComponent } from '@/Types/components'
+import { SizeWatcher } from './SizeWatcher'
 
 interface ModalProps extends TransitionEvents {
   isOpen: boolean
@@ -28,23 +29,30 @@ export const Modal: ParentComponent<ModalProps> = ({
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        <SizeWatcher isFullScreen>
+          {({ width }) => (
+            <div
+              className="fixed inset-0 z-10 mx-auto overflow-y-auto"
+              style={{ maxWidth: width }}
             >
-              <Dialog.Panel className="nes-dialog is-rounded street w-full text-left">
-                {children}
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="nes-dialog is-rounded street w-full text-left">
+                    {children}
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          )}
+        </SizeWatcher>
       </Dialog>
     </Transition.Root>
   )
